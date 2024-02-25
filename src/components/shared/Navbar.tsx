bbf54313e2edd577ac4e5b346fc50e18d518daf3
Arpon <a4arpon@gmail.com>
2024-02-26 00:36:56 +0600

    mega menu added

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,9 +1,11 @@
 import lpaImageLogo from "@/assets/LPA-solo_square-150x150 1.png"
 import { Link } from "@tanstack/react-router"
-import { AlignJustify, ShoppingBag } from "lucide-react"
+import { AlignJustify, ShoppingBag, X } from "lucide-react"
+import { useState } from "react"
 import Button from "./Button"
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
   return (
     <header className="border-b border-light/20">
       <nav className="container flex justify-between py-[6px] manrope-font items-center">
@@ -42,10 +44,63 @@ const Navbar = () => {
             <ShoppingBag />
           </Button>
         </div>
-        <div className=" lg:hidden">
+        <button
+          type="button"
+          onClick={() => setIsNavOpen(true)}
+          className=" lg:hidden"
+        >
           <AlignJustify />
-        </div>
+        </button>
       </nav>
+      {isNavOpen && (
+        <div className="fixed p-3 top-0 h-full bg-dark w-full z-20">
+          <div className="flex gap-3 justify-between items-center">
+            <h2 className="text-xl font-bold">LA PETITE AGORA</h2>
+            <div className="">
+              <button
+                type="button"
+                className="text-light bg-light/20 rounded-full p-2 focus:scale-95 ease-in-out duration-200 transition-all"
+                onClick={() => setIsNavOpen(false)}
+              >
+                <X />
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 mt-10">
+            <div className="flex flex-col gap-2 relative">
+              <Link
+                to="/"
+                onClick={() => setIsNavOpen(false)}
+                className="text-light font-semibold text-base"
+              >
+                Accueil
+              </Link>
+              <div className="h-[1px] w-full gradient-bg-1" />
+            </div>
+            <Link
+              to="/"
+              onClick={() => setIsNavOpen(false)}
+              className="text-light/80 font-semibold text-base"
+            >
+              Services
+            </Link>
+            <Link
+              to="/"
+              onClick={() => setIsNavOpen(false)}
+              className="text-light/80 font-semibold text-base"
+            >
+              Abonnements
+            </Link>
+            <Link
+              to="/"
+              onClick={() => setIsNavOpen(false)}
+              className="text-light/80 font-semibold text-base"
+            >
+              FAQ
+            </Link>
+          </div>
+        </div>
+      )}
     </header>
   )
 }
